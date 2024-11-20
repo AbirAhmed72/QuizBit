6438bad62878a90e12263e3714b397806807bd39
AbirAhmed72 <abirahmed14872@gmail.com>
2024-11-20 22:48:17 +0600

    added system design diagrams

--- quizbit/quiz/views.py
+++ quizbit/quiz/views.py
@@ -1,24 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import AnswerChoice, Question, PracticeHistory
 from django.contrib.auth.models import User
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
 
-
-# class QuestionListAPI(APIView):
-#     def get(self, request):
-#         questions = Question.objects.all().values('id', 'title', 'choices')
-#         return Response(list(questions), status=status.HTTP_200_OK)
 
 class QuestionListAPI(APIView):
+    """
+    API to retrieve a list of all questions and their answer choices.
+    """
     permission_classes = [AllowAny]
 
     def get(self, request):
@@ -38,18 +33,10 @@ class QuestionListAPI(APIView):
         return Response(data, status=status.HTTP_200_OK)
 
 
-# class QuestionDetailAPI(APIView):
-#     def get(self, request, question_id):
-#         question = get_object_or_404(Question, id=question_id)
-#         data = {
-#             "id": question.id,
-#             "title": question.title,
-#             "description": question.description,
-#             "choices": question.choices,
-#         }
-#         return Response(data, status=status.HTTP_200_OK)
-    
 class QuestionDetailAPI(APIView):
+    """
+    API to retrieve details of a specific question by ID.
+    """
     permission_classes = [AllowAny]
 
     def get(self, request, question_id):
@@ -69,25 +56,10 @@ class QuestionDetailAPI(APIView):
         return Response(data, status=status.HTTP_200_OK)
 
 
-# class SubmitAnswerAPI(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request, question_id):
-#         question = get_object_or_404(Question, id=question_id)
-#         submitted_answer = request.data.get('submitted_answer')
-#         if not submitted_answer:
-#             return Response({"error": "Submitted answer is required."}, status=status.HTTP_400_BAD_REQUEST)
-
-#         is_correct = submitted_answer == question.correct_answer
-#         PracticeHistory.objects.create(
-#             user=request.user,
-#             question=question,
-#             submitted_answer=submitted_answer,
-#             is_correct=is_correct,
-#         )
-#         return Response({"is_correct": is_correct}, status=status.HTTP_200_OK)
-
 class SubmitAnswerAPI(APIView):
+    """
+    API to submit an answer for a specific question.
+    """
     permission_classes = [IsAuthenticated]
 
     def post(self, request, question_id):
@@ -111,7 +83,11 @@ class SubmitAnswerAPI(APIView):
         )
         return Response({"is_correct": is_correct}, status=status.HTTP_200_OK)
 
+
 class UserPracticeHistoryAPI(APIView):
+    """
+    API to retrieve the practice history of the authenticated user.
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -121,11 +97,12 @@ class UserPracticeHistoryAPI(APIView):
         return Response(list(history), status=status.HTTP_200_OK)
 
 
-
-
-
 class RegisterUserAPI(APIView):
+    """
+    API to register a new user.
+    """
     permission_classes = [AllowAny]
+
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -144,8 +121,13 @@ class RegisterUserAPI(APIView):
             status=status.HTTP_201_CREATED,
         )
 
+
 class LoginUserAPI(APIView):
+    """
+    API to log in an existing user.
+    """
     permission_classes = [AllowAny]
+
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -161,6 +143,3 @@ class LoginUserAPI(APIView):
         token, _ = Token.objects.get_or_create(user=user)
 
         return Response({"message": "Login successful!", "token": token.key}, status=status.HTTP_200_OK)
-
-
-
